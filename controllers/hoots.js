9972f208f12dc42d275afb868183d4b045ca32bc
realpolya <ps2780@columbia.edu>
2024-10-23 12:17:06 -0400

    created update, delete and create comment routes

--- controllers/hoots.js
+++ controllers/hoots.js
@@ -3,19 +3,73 @@
 import Hoot from '../models/model-hoot.js';
 
 
-
 /* --------------------------------GET Controllers--------------------------------*/
 
 
 /* --------------------------------POST Controllers--------------------------------*/
 
+const createComment = async (req, res) => {
+
+    try {
+
+        // find the hoot by id
+        const hoot = await Hoot.findById(req.params.hootId);
+
+        // define the author
+        req.body.author = req.user._id;
+
+        // push the comment
+        hoot.comments.push(req.body);
+
+        // save
+        await hoot.save();
+
+        // return
+        res.status(201).json({ hoot });
+
+    } catch(error) {
+
+        res.status(400).json({ error: err.message });
+
+    }
+
+}
 
 /* --------------------------------PUT Controllers--------------------------------*/
 
+const updateHoot = async (req, res) => {
+
+    try {
+
+        const hoot = await Hoot.findByIdAndUpdate(req.params.hootId, req.body);
+        res.status(201).json({ hoot });
+
+    } catch(err) {
+
+        res.status(400).json({ error: err.message });
+
+    }
+
+}
 
 /* --------------------------------DELETE Controllers--------------------------------*/
 
+const deleteHoot = async (req, res) => {
+
+    try {
+
+        const hoot = await Hoot.findByIdAndDelete(req.params.hootId);
+        const hoots = await Hoot.find();
+        res.status(201).json({ "deleted": hoot, hoots });
+
+    } catch(err) {
+
+        res.status(400).json({ error: err.message });
+
+    }
+
+}
 
 /* --------------------------------Exports--------------------------------*/
 
-export {  }
+export { createComment, updateHoot, deleteHoot }
